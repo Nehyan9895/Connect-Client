e21e3728ab184296eb3f5196d43536af16574394
Muhammed Nehyan M <muhammednehyan9895@gmail.com>
2024-08-03 20:26:42 +0530

    route changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/candidate/home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:HomeComponent
-    }
+
 ];
