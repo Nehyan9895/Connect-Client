0a39a6e4ac849b20498bf49c4d18144587b5c2e4
Muhammed Nehyan M <muhammednehyan9895@gmail.com>
2024-08-04 19:18:05 +0530

    checked if any withCredential is there

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { candidateRoute } from './routes/candidate.route';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+import { routes } from './app.routes';
+import { candidateRoute } from './routes/candidate.route';
 import { recruiterRoute } from './routes/recruiter.route';
 import { sharedRoute } from './routes/shared.route';
 import { adminRoute } from './routes/admin.route';
 import { userAuthInterceptor } from './services/interceptor/user-auth.interceptor';
 // import { recruiterAuthInterceptor } from './services/interceptor/recruiter-auth.interceptor';
-import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
-
 
 const config: SocketIoConfig = { url: 'http://connect.clothingcue.store', options: {} };
 
@@ -39,9 +38,8 @@ export const appConfig: ApplicationConfig = {
       progressBar: true, 
       timeOut: 3000, 
       progressAnimation: 'decreasing' 
-
-    }), provideAnimationsAsync(), provideAnimationsAsync(),
+    }),
+    provideAnimationsAsync(),
     importProvidersFrom(SocketIoModule.forRoot(config)),  
-    
   ]
 };
